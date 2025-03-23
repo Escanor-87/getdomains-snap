78f148fae6f7aaf19fa20b18d167ca18d201195b
Escanor-87 <94392088+Escanor-87@users.noreply.github.com>
2025-03-23 15:05:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 printf "\033[32;1mInstalling packeges\033[0m\n"
-apk update && apk add curl kmod-nft-tproxy xray-core 
+opkg update && opkg install curl kmod-nft-tproxy xray-core 
 
 printf "\033[32;1mDownloading config.json\033[0m\n"
-curl -Lo /etc/xray/config.json https://raw.githubusercontent.com/FiyeroT/getdomains-snap/main/config.json
+curl -Lo /etc/xray/config.json https://raw.githubusercontent.com/Davoyan/router-xray-fakeip-installation/main/config.json
 
 printf "\033[32;1mEnabling xray service\033[0m\n"
 uci set xray.enabled.enabled='1'
@@ -14,7 +14,7 @@ service xray enable
 printf "\033[32;1mConfiguring update_domains script\033[0m\n"
 echo "#!/bin/sh" > /etc/xray/update_domains.sh
 echo "set -e" >> /etc/xray/update_domains.sh
-echo "curl -Lo /usr/share/xray/allow-domains.dat https://github.com/unidcml/allow-domains-dat/releases/latest/download/allow-domains.dat" >> /etc/xray/update_domains.sh
+echo "curl -Lo /usr/share/xray/refilter.dat https://github.com/1andrevich/Re-filter-lists/releases/latest/download/geosite.dat" >> /etc/xray/update_domains.sh
 echo "service xray restart" >> /etc/xray/update_domains.sh
 chmod +x /etc/xray/update_domains.sh
 
@@ -37,6 +37,10 @@ uci -q delete dhcp.@dnsmasq[0].server
 uci add_list dhcp.@dnsmasq[0].server="127.0.0.1#5353"
 uci commit dhcp
 
+RC_LOCAL="/etc/rc.local"
+grep -q "ethtool -K eth0 tso off" "$RC_LOCAL" || sed -i "/^exit 0/i ethtool -K eth0 tso off" "$RC_LOCAL"
+grep -q "(sleep 10 && service xray restart)" "$RC_LOCAL" || sed -i "/^exit 0/i (sleep 10 && service xray restart)" "$RC_LOCAL"
+grep -q "(sh && /etc/xray/update_domains.sh) &" "$RC_LOCAL" || sed -i "/^exit 0/i (sh && /etc/xray/update_domains.sh) &" "$RC_LOCAL"
 
 printf "\033[32;1mConfigure network\033[0m\n"
 rule_id=$(uci show network | grep -E '@rule.*name=.mark0x1.' | awk -F '[][{}]' '{print $2}' | head -n 1)
@@ -60,18 +64,46 @@ if [ ! -z "$rule_id2" ]; then
     while uci -q delete firewall.@rule[$rule_id2]; do :; done
 fi
 
+uci add firewall rule
+uci set firewall.@rule[-1].name='Block UDP 443'
+uci set firewall.@rule[-1].src='lan'
+uci set firewall.@rule[-1].dest='*'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].dest_port='443'
+uci set firewall.@rule[-1].target='DROP'
+uci set firewall.@rule[-1].family='ipv4'
 
 uci add firewall rule
 uci set firewall.@rule[-1]=rule
 uci set firewall.@rule[-1].name='Fake IP via proxy'
 uci set firewall.@rule[-1].src='lan'
 uci set firewall.@rule[-1].dest='*'
-uci set firewall.@rule[-1].dest_ip='100.96.0.0/16'
+uci set firewall.@rule[-1].dest_ip='198.18.0.0/15'
 uci add_list firewall.@rule[-1].proto='tcp'
 uci add_list firewall.@rule[-1].proto='udp'
 uci set firewall.@rule[-1].target='MARK'
 uci set firewall.@rule[-1].set_mark='0x1'
 uci set firewall.@rule[-1].family='ipv4'
+
+uci add firewall rule
+uci set firewall.@rule[-1].name='Discord Voice via proxy'
+uci set firewall.@rule[-1].src='lan'
+uci set firewall.@rule[-1].dest='*'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].target='MARK'
+uci set firewall.@rule[-1].set_mark='0x1'
+uci set firewall.@rule[-1].family='ipv4'
+uci set firewall.@rule[-1].dest_port='50000-51000'
+uci add_list firewall.@rule[-1].dest_ip='138.128.136.0/21'
+uci add_list firewall.@rule[-1].dest_ip='162.158.0.0/15'
+uci add_list firewall.@rule[-1].dest_ip='172.64.0.0/13'
+uci add_list firewall.@rule[-1].dest_ip='34.0.0.0/15'
+uci add_list firewall.@rule[-1].dest_ip='34.2.0.0/15'
+uci add_list firewall.@rule[-1].dest_ip='35.192.0.0/12'
+uci add_list firewall.@rule[-1].dest_ip='35.208.0.0/12'
+uci add_list firewall.@rule[-1].dest_ip='5.200.14.128/25'
+uci add_list firewall.@rule[-1].dest_ip='66.22.192.0/18'
+
 uci commit firewall
 
 echo "chain tproxy_marked {" > /etc/nftables.d/30-xray-tproxy.nft
@@ -79,6 +111,4 @@ echo "  type filter hook prerouting priority filter; policy accept;" >> /etc/nft
 echo "  meta mark 0x1 meta l4proto { tcp, udp } tproxy ip to 127.0.0.1:12701 counter accept" >> /etc/nftables.d/30-xray-tproxy.nft
 echo "}" >> /etc/nftables.d/30-xray-tproxy.nft
 
-service dnsmasq restart && service network restart && service firewall restart
-
-
+service xray restart && service dnsmasq restart && service network restart && service firewall restart
