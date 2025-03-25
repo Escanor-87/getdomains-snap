34f8c7c0dd70b4caadc4250aa2c91467d5213358
Escanor-87 <94392088+Escanor-87@users.noreply.github.com>
2025-03-25 21:48:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf "\033[32;1mInstalling packeges\033[0m\n"
-apk update && apk install curl kmod-nft-tproxy xray-core 
+apk update && apk add curl kmod-nft-tproxy xray-core 
 
 printf "\033[32;1mDownloading config.json\033[0m\n"
 curl -Lo /etc/xray/config.json https://raw.githubusercontent.com/Davoyan/router-xray-fakeip-installation/main/config.json
